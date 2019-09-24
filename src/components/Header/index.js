0d26819af6cdf09f45bd2772dbb818d5698f8bc9
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2019-09-24 20:53:08 -0300

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css'
+
+//Stateless components
+//cria componentes no react com funções
+const Header = () => <header id='main-header'>JSHunt</header>;
+
+
+export default Header;
+
